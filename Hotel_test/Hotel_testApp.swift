295f6e6c60127621786464e3138aab75eba5b952
KudryashovAlexander <sasha080193@gmail.com>
2023-12-17 21:21:57 +0300

    Initial Commit

---
+++ Hotel_test/Hotel_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hotel_testApp.swift
+//  Hotel_test
+//
+//  Created by Александр Кудряшов on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Hotel_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
