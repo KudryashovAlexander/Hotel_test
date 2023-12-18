8404e4d97798cc60cc92e7fbb0165f3705a9e828
KudryashovAlexander <sasha080193@gmail.com>
2023-12-18 11:45:24 +0300

    add button to resultView

---
+++ Hotel_test/Scene/ResultOrder/ResultOrderView.swift
@@ -0,0 +1,82 @@
+//
+//  ResultOrderView.swift
+//  Hotel_test
+//
+//  Created by Александр Кудряшов on 17.12.2023.
+//
+
+import SwiftUI
+
+struct ResultOrderView: View {
+    @StateObject var viewModel: ResultOrderViewModel
+    var body: some View {
+        VStack {
+            VStack {
+                Spacer()
+                image
+                texts
+                Spacer()
+            }
+            .padding(.horizontal, Constants.padding)
+            Divider()
+            button
+        }
+    }
+    
+    private var image: some View {
+        ZStack {
+            Circle()
+                .frame(width: Constants.Circle.radius, height: Constants.Circle.radius)
+                .foregroundColor(.hLightGrayPhone)
+            Image(viewModel.imageName)
+        }
+    }
+    
+    private var texts: some View {
+        VStack {
+            Text(viewModel.orderResult)
+                .font(.Medium.size22)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.hBlack)
+                .padding(.top, Constants.OrderResult.topPadding)
+                .padding(.bottom, Constants.OrderResult.bottomPadding)
+            Text(viewModel.orderComment)
+                .font(.Regular.size16)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.gray)
+        }
+    }
+    
+    private var button: some View {
+        VStack {
+            Button(viewModel.buttonName) {
+                print("press Button")
+            }
+            .frame(maxWidth: .infinity, maxHeight: Constants.Button.height)
+            .foregroundColor(.white)
+            .background(Color.hBlue)
+            .cornerRadius(Constants.Button.cornerRadius)
+        }
+        .padding(.horizontal, Constants.Button.padding)
+    }
+    
+    private enum Constants {
+        static let padding: CGFloat = 23
+        enum Circle {
+            static let radius: CGFloat = 94
+        }
+        enum OrderResult {
+            static let topPadding: CGFloat = 32
+            static let bottomPadding: CGFloat = 20
+        }
+        enum Button {
+            static let height: CGFloat = 48
+            static let cornerRadius: CGFloat = 15
+            static let padding: CGFloat = 16
+        }
+    }
+}
+
+#Preview {
+    ResultOrderView(viewModel: ResultOrderViewModel())
+}
---
+++ Hotel_test/Scene/ResultOrder/ResultOrderViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  ResultOrderViewModel.swift
+//  Hotel_test
+//
+//  Created by Александр Кудряшов on 18.12.2023.
+//
+
+import Foundation
+
+protocol ResultOrderProtocol: ObservableObject  {
+    var imageName: String { get }
+    var orderResult: String  { get }
+    var orderComment: String { get }
+    var buttonName : String { get }
+}
+
+class ResultOrderViewModel: ResultOrderProtocol {
+    @Published var imageName: String = "sucessOrder"
+    @Published var orderResult: String = "Ваш заказ принят в работу"
+    @Published var orderComment: String = "Подтверждение заказа №104893 может занять некоторое время (от 1 часа до суток). Как только мы получим ответ от туроператора, вам на почту придет уведомление."
+    @Published var buttonName: String = "Супер!"
+}
